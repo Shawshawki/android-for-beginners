e723d5188005a8fff913717b357c2ed3ba893e17
pandaguo <panda.guo@gservfocus.com>
2017-03-30 13:31:25 +0800

    L1作业布局相关

---
+++ L1_homewrok/panda/LayoutDemo20170327/app/src/main/java/com/example/pandaguo/layoutdemo20170327/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.pandaguo.layoutdemo20170327;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
